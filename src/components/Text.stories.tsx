7012127464c5f937b1781850c029c0efb0afc4c4
Hyago Rios <hyagorioss@gmail.com>
2022-10-15 20:57:08 -0400

    Ignite Lab - React DS
    Final Files

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from "./Text";
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Lorem ipsum.',
+        size: 'md',
+    }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {
+    args: {
+        size: 'md'
+    }
+}
+
+export const Small: StoryObj<TextProps> = {
+    args: {
+        size: 'sm'
+    }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: {
+        size: 'lg'
+    }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: {
+        asChild: true,
+        children: (
+            <p>Text with P</p>
+        )
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true, 
+            }
+        },
+        asChild: {
+            table: {
+                disable: true, 
+            }
+        }
+    }
+}
